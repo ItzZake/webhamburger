8cc146fb8837022445112d7059ad4599ae029ba2
Ahmed <135468643+CrazoFrazo@users.noreply.github.com>
2025-12-15 00:34:30 +0200

    Update Footer.php

--- About us/Footer.php
+++ About us/Footer.php
@@ -8,9 +8,9 @@
             <ul>
                 <li><a href="../Home Full/Home.php">Home</a></li>
                 <li><a href="Store.html">Store</a></li>
-                <li><a href="Aboutus.php">About Us</a></li>
+                <li><a href="../About us/Aboutus.php">About Us</a></li>
                 <li><a href="FAQ.html">FAQ</a></li>
-                <li><a href="Contact.html">Contact Us</a></li>
+                <li><a href="../Contact us/contact.php">Contact Us</a></li>
             </ul>
         </div>
 
